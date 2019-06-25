3b5615fe8f0dabf8db144bddc0a263c7c75a0d39
Dung <thanhtai9606>
2019-06-25 16:59:22 +0700

    update Initial Database Context

--- FE.Advanture/FE.Advanture.Api/Program.cs
+++ FE.Advanture/FE.Advanture.Api/Program.cs
@@ -16,8 +16,25 @@ namespace FE.Advanture.Api
     {
         public static void Main(string[] args)
         {
-            
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<UniversityContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
